2f991f1ff3f0a81cd52e819b256311f95093f088
dominosski <domix852@gmail.com>
2021-04-03 16:57:35 +0200

    Added get method to show page with customers included in db

---
+++ src/main/java/com/DAO/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.DAO;
+
+import java.util.List;
+
+import com.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
---
+++ src/main/java/com/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.DAO.CustomerDAO;
+import com.entity.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@RequestMapping("/list")
+	public String listCustomers(Model model)
+	{
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		model.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+}
